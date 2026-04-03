128eca92a4538bd3fa3055c37155527ac22043b8
rakib3719 <bannah76769@gmail.com>
2026-04-03 16:58:32 +0600

    Feature: Subscription page complate

--- frontend/app/pricing/page.tsx
+++ frontend/app/pricing/page.tsx
@@ -1,9 +1,11 @@
+import Subscription from '@/components/website/home/Subscription';
 import PricingBanner from './PricingBanner';
 
 const page = () => {
   return (
     <div>
       <PricingBanner/>
+      <Subscription/>
       
     </div>
   );
