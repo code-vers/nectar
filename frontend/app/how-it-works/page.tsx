f384d81ee960de5a91687c793b71373fe075c89d
rakib3719 <bannah76769@gmail.com>
2026-04-03 15:42:36 +0600

    Feature: How Its Work create

---
+++ frontend/app/how-it-works/page.tsx
@@ -0,0 +1,12 @@
+import HIWBanner from '@/components/website/howItsWorks/HIWBanner';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <HIWBanner/>
+    </div>
+  );
+};
+
+export default page;
---
+++ frontend/components/website/howItsWorks/HIWBanner.tsx
@@ -0,0 +1,40 @@
+import BannerText from "../typography/BannerText";
+import PrimaryButton from "../typography/PrimaryButton";
+import SecondaryButton from "../typography/SecondaryButton";
+
+const HIWBanner = () => {
+  return (
+    <section className="relative w-full overflow-hidden h-[calc(100vh-80px)] min-h-140">
+      <video
+        className="absolute inset-0 w-full h-full object-cover"
+        src="/video/banner_video.mp4"
+        autoPlay
+        muted
+        loop
+        playsInline
+      />
+
+      <div className="absolute inset-0 bg-black/55" />
+
+      <div className="relative z-10 h-full max-w-360 mx-auto px-6 sm:px-8 lg:px-10 flex flex-col justify-center">
+        <BannerText
+       
+          title="How the Platform Works
+"
+   
+          description="A simple system that helps property owners, managers, and residents build better communities through training, tools, and shared learning.
+"
+        />
+
+        <div className="flex flex-col sm:flex-row gap-3 mt-6">
+          <PrimaryButton href="/membership" className="min-w-45" isBanner={true}>
+            Start Membership
+          </PrimaryButton>
+      
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HIWBanner;
