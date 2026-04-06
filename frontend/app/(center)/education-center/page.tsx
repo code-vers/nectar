f47061faa0d13b3d3af7c588fb8d56bbc3d0990e
rakib3719 <bannah76769@gmail.com>
2026-04-06 12:15:57 +0600

    Featue: Feature Course update

--- frontend/app/(center)/education-center/page.tsx
+++ frontend/app/(center)/education-center/page.tsx
@@ -7,7 +7,7 @@ import NewsLatter from '@/components/website/center/NewsLatter';
 
 const page = () => {
   return (
-    <div className='bg-main-bg'>
+    <div className='bg-main-bg '>
       <CenterBanner
         heading="Owner / Landlord Education Center Professional courses, practical"
         description="Professional courses, practical tools, and expert guidance designed to help property owners manage properties, support residents, and grow their portfolio."
@@ -19,11 +19,14 @@ const page = () => {
           text: "Explore More",
           href: "/courses"
         }}
-      />
-<CenterFeaturedCourses/>
+      /> 
+<div className='max-w-370 mx-auto'>
+
+  <CenterFeaturedCourses/>
       <CenterCategory/>
       <CenterResource/>
       <NewsLatter/>
+</div>
     </div>
   );
 };
