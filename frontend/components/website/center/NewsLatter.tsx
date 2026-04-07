02e9dc177d2b092810ba6beb71a8a876be33c628
rakib3719 <bannah76769@gmail.com>
2026-04-07 14:09:24 +0600

    Feature: Dashboard Initilization

--- frontend/components/website/center/NewsLatter.tsx
+++ frontend/components/website/center/NewsLatter.tsx
@@ -1,25 +1,21 @@
-import PrimaryButton from '../typography/PrimaryButton';
-import SectionHeading from '../typography/SectionHeading';
+import PrimaryButton from "../typography/PrimaryButton";
+import SectionHeading from "../typography/SectionHeading";
 
 const NewsLatter = () => {
   return (
     <div className='bg-[#EFF9E4] px-4 p-16'>
+      <SectionHeading
+        title='Grow Your Property Management Skills
+'
+        description='Access professional training, tools, and guides designed for modern property owners.
+'
+      />
 
-    <SectionHeading title='Grow Your Property Management Skills
-' description='Access professional training, tools, and guides designed for modern property owners.
-'/>
+      <div className='text-center flex gap-8 justify-center pt-8'>
+        <PrimaryButton className='py-2'>Start Membership</PrimaryButton>
 
-
-<div className='text-center flex gap-8 justify-center pt-8'>
-  <PrimaryButton className='py-2'>
-  Start Membership
-</PrimaryButton>
-
-<PrimaryButton className='bg-green py-2'>
-  Explore Courses
-</PrimaryButton>
-</div>
-      
+        <PrimaryButton className='bg-green py-2'>Explore Courses</PrimaryButton>
+      </div>
     </div>
   );
 };
