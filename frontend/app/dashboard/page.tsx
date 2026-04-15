b2a0cd124ab3e1e677298850c500849e8920d0f6
rakib3719 <bannah76769@gmail.com>
2026-04-15 11:32:53 +0600

    Feat: Add Vendor Success Path Overview component and integrate it into the dashboard

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,5 +1,11 @@
+import VendorDashboard from "@/components/dashboard/vendor/dashboardHome/VendorDashboard";
+
 const page = () => {
-  return <div className='space-y-8'></div>;
+  return (
+    <div className='space-y-8'>
+      <VendorDashboard />
+    </div>
+  );
 };
 
 export default page;
