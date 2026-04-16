4c70611dc9e956a77dde55c8cafcca22c88fa3b6
rakib3719 <bannah76769@gmail.com>
2026-04-16 10:15:30 +0600

    Fix: Home Page Resonsive issue solved

--- frontend/components/website/home/HowItsFor.tsx
+++ frontend/components/website/home/HowItsFor.tsx
@@ -2,14 +2,15 @@ import SectionHeading from "../typography/SectionHeading";
 import FeatureCards from "./FeatureCards";
 import WhatYouGet from "./WhatYouGet";
 
-
 const HowItsFor = () => {
   return (
-    <div className="pt-20 ">
-      <SectionHeading title="Who It's For" description="A learning platform designed for everyone in the property ecosystem"/>
-      <FeatureCards/>
-      <WhatYouGet/>
-
+    <div className='pt-20 '>
+      <SectionHeading
+        title="Who It's For"
+        description='A learning platform designed for everyone in the property ecosystem'
+      />
+      <FeatureCards />
+      <WhatYouGet />
     </div>
   );
 };
