16c15a05df33dc53011137a47aede9c7eebfa445
rakib3719 <bannah76769@gmail.com>
2026-04-20 10:55:56 +0600

    feat: enhance UpdateCourseDto with additional properties and validation

--- backend/src/modules/course/dto/update-course.dto.ts
+++ backend/src/modules/course/dto/update-course.dto.ts
@@ -1,4 +1,37 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsArray, IsString } from 'class-validator';
+import {
+  CourseCategory,
+  CourseStatus,
+  Level,
+} from 'src/common/enums/courses.enum';
 import { CreateCourseDto } from './create-course.dto';
 
-export class UpdateCourseDto extends PartialType(CreateCourseDto) {}
+export class UpdateCourseDto extends PartialType(CreateCourseDto) {
+  @IsString()
+  course_title?: string;
+
+  @IsString()
+  course_description?: string;
+
+  @IsString()
+  category?: CourseCategory;
+
+  @IsString()
+  status?: CourseStatus;
+
+  @IsString()
+  course_type?: Level;
+
+  @IsString()
+  course_banner?: string;
+
+  @IsString()
+  total_hours?: number;
+
+  @IsString()
+  total_enroll?: number;
+
+  @IsArray()
+  content?: object[];
+}
